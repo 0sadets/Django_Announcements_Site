1d9ea4ddef5c114b57faaac00d1557558e631902
0sadets <102857086+0sadets@users.noreply.github.com>
2025-01-18 12:30:44 +0200

    add changes

--- project/announcements/models.py
+++ project/announcements/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=512)
+    icon = models.CharField(max_length=2083)
+
+    def __str__(self):
+        return self.title
+    
+class Announcement(models.Model):
+    title = models.CharField(max_length=512)
+    category_id = models.IntegerField()
+    image = models.CharField(max_length=2083)
+    price = models.IntegerField()
+    location = models.CharField(max_length=512)
+    desc = models.CharField(max_length=2083)
+    is_active = models.BooleanField(default=True) 
+    created_at = models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
--- project/announcements/views.py
+++ project/announcements/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from announcements.models import Category
+from announcements.models import Announcement
 
-# Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    return render(request, "index.html", {"categories": categories})
+
+def list(request, id):
+    announcements = Announcement.objects.filter(category_id=id)
+    return render(request, "list.html", {"announcements": announcements})
+
+def delete(request, id):
+    announcements = get_object_or_404(Announcement, id=id)
+    
+    announcements.delete()
+    return redirect("/")
